f6776ea3c5f07fd44bfd6b0cbe95de39c809440f
s20246 <s20246@pjwstk.edu.pl>
2022-06-04 14:20:57 +0200

    dwa widoki, button

--- app/src/main/java/com/example/roomapp/fragments/add/AddFragment.kt
+++ app/src/main/java/com/example/roomapp/fragments/add/AddFragment.kt
@@ -1,10 +1,12 @@
 package com.example.roomapp.fragments.add
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.roomapp.R
 
 class AddFragment : Fragment() {
@@ -16,4 +18,5 @@ class AddFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_add, container, false)
     }
+
 }
